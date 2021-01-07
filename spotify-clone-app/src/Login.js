9272c334858c8e345c8b999312a8fd79bd893fe1
Adrian.Maddix <amaddix2012@gmail.com>
2021-01-07 08:04:06 +0000

    removed text on line 7 in Login.js file

--- spotify-clone-app/src/Login.js
+++ spotify-clone-app/src/Login.js
@@ -3,8 +3,7 @@ import "./Login.css"
 
 function Login() {
     return (
-        <div className="login">
-            <h1>I am the login page</h1>
+        <div className="login">       
             <img
         src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
         alt=""
