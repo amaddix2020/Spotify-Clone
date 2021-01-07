0e2d1047d53df11d662662bf3dc59cf6562337db
Adrian.Maddix <amaddix2012@gmail.com>
2021-01-07 12:14:23 +0000

    updated App.js file, added usestate hook function in line 7

--- spotify-clone-app/src/App.js
+++ spotify-clone-app/src/App.js
@@ -1,13 +1,16 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Login from './Login';
 import { getTokenFromUrl } from './spotify';
 
 function App() {
-
+  const [token, setToken] = useState(null);
 
   useEffect(() => {
    const token = getTokenFromUrl();
+   window.location.hash = "";
+   const token = hash.access_token;
+
    console.log('I HAVE A TOKEN>>>', token )
   }, []);
 
