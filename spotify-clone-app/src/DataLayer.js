77b63934b189105207eb55983132bc3e52bdb8fc
Adrian.Maddix <amaddix2012@gmail.com>
2021-01-08 12:48:33 +0000

    updated DataLayer.js file, entered text in file

--- spotify-clone-app/src/DataLayer.js
+++ spotify-clone-app/src/DataLayer.js
@@ -1,15 +1,11 @@
-import React,
-{ createContext, 
-  useContext,
-  UseReducer } 
-  from "react";
+import React, { createContext, useContext, UseReducer } from "react";
 
   export const DataLayerContext = createContext();
 
   export const DataLayer = ({ initialState, reducer, children }) => (
-    <StateContext.Provider value={useReducer(reducer, initialState)}>
+    <DataLayerContext.Provider value={useReducer(reducer, initialState)}>
       {children}
-    </StateContext.Provider>
+    </DataLayerContext.Provider>
   );
   
   export const useStateValue = () => useContext(StateContext);
