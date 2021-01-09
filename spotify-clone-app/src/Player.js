111d36ef3c5cb52cd5fecb5d81d5c87e433529eb
Adrian.Maddix <amaddix2012@gmail.com>
2021-01-09 04:21:31 +0000

    updated Player.js file,rendered the Body component in player.js

--- spotify-clone-app/src/Player.js
+++ spotify-clone-app/src/Player.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import "./Player.css";
 import Sidebar from './Sidebar';
+import Body from './Body';
 
 function Player({ spotify}) {
     return (
         <div className="player">
             <div className="player__body">
             <Sidebar />
+            <Body />
+           
             </div>
             
         </div>
