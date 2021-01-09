e36329974dcf77b161a142d2bf00f48c71622247
Adrian.Maddix <amaddix2012@gmail.com>
2021-01-09 04:11:57 +0000

    updated Rendered the Sidebar component in Player.js

--- spotify-clone-app/src/Player.js
+++ spotify-clone-app/src/Player.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import "./Player.css";
+import Sidebar from './Sidebar';
 
 function Player({ spotify}) {
     return (
         <div className="player">
             <div className="player__body">
-                
+            <Sidebar />
             </div>
             
         </div>
